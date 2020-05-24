cb5b3f318689711caefc4f953e69086ae6955f6c
enkronan <robin@tempura.se>
2020-05-24 23:01:58 +0200

    type: feature
    added styling to main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import MovieList from './movieList';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <MovieList/>
     </div>
   );
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Nav = ()  => {
+
+    return(
+        <div className="nav">
+        <h3>Robin Moe</h3>
+        <p>Amount of movies:</p>
+        </div>
+    );
+}
+
+
+export default Nav;
