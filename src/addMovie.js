4c4aded4222aa1d7545cd3f4210924f7d604d177
enkronan <robin@tempura.se>
2020-05-28 19:24:33 +0200

    type: feature
    addMovie now adds another movie to the previous state

--- src/addMovie.js
+++ src/addMovie.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import {MovieContext} from './MovieContext';
 
 const AddMovie = () => {
+
+    const [movies, setMovies] = useContext(MovieContext);
 
     const [name, setName] = useState('');
     const [price, setPrice] = useState('');
@@ -15,12 +18,15 @@ const AddMovie = () => {
 
     const addMovie = e => {
         e.preventDefault();
+        setMovies(prevMovies => [...prevMovies, {name:name, price:price}]);
     }
     return(
-        <form>
+        <form onSubmit={addMovie}>
             <input type="text" name="name" value={name} onChange={updateName}/>
             <input type="text" name="price" value={price} onChange={updatePrice}/>
             <button>Submit</button>
         </form>
     )
-}
+};
+
+export default AddMovie;
