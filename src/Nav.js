65eaebbca76d920ced682be2f5229361b75c95a4
enkronan <robin@tempura.se>
2020-05-28 19:09:01 +0200

    type: feature
    importing MovieContext to Nav and showing length of movie array in nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useCallback, useContext } from "react";
+import {MovieContext} from './MovieContext';
+
 
 const Nav = ()  => {
+
+    const [movies, setMovies] = useContext(MovieContext);
 
     return(
         <div className="nav">
         <h3>Robin Moe</h3>
-        <p>Amount of movies:</p>
+        <p>Amount of movies: {movies.length}</p>
         </div>
     );
 }
